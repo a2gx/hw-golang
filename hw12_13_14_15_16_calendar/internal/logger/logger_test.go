c7337dd4d9e64a62f2f5dd5ea62ec4b4bfcc405f
Alex Buylov <a.buylov@pnlfin.tech>
2025-04-03 18:45:46 +0300

    Hw12 Calendar && fix logger

--- hw12_13_14_15_16_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger_test.go
@@ -1,7 +1,43 @@
 package logger
 
-import "testing"
+import (
+	"log/slog"
+	"os"
+	"testing"
+)
 
 func TestLogger(t *testing.T) {
-	// TODO
+	logg, closeFn := New("debug", Options{
+		Handler: "json",
+	})
+	defer closeFn()
+
+	if logg == nil {
+		t.Fatal("logger is nil")
+	}
+}
+
+func TestParseLevel(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected slog.Level
+	}{
+		{"debug", slog.LevelDebug},
+		{"info", slog.LevelInfo},
+		{"warn", slog.LevelWarn},
+		{"error", slog.LevelError},
+	}
+
+	for _, test := range tests {
+		if got := parseLevel(test.input); got != test.expected {
+			t.Errorf("parseLevel(%q) = %v; want %v", test.input, got, test.expected)
+		}
+	}
+}
+
+func TestCreateWriter(t *testing.T) {
+	w, _ := createWriter(Options{Handler: "text"})
+	if w != os.Stdout {
+		t.Errorf("createWriter({handler}) = %v; want os.Stdout", w)
+	}
 }
