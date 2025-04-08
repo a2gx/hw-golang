03e04907a2f03a7bd15d6934409dc4f764a7ccd3
Alex Buylov <a.buylov@pnlfin.tech>
2025-04-08 16:09:50 +0300

    Hw12 in progress

--- hw12_13_14_15_16_calendar/internal/app/interfaces.go
+++ hw12_13_14_15_16_calendar/internal/app/interfaces.go
@@ -1,10 +1,21 @@
 package app
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type Storage interface {
 	Connect(ctx context.Context) error
 	Close(ctx context.Context) error
+
+	CreateEvent(event Event) (Event, error)
+	UpdateEvent(event Event) (Event, error)
+	DeleteEvent(event Event) error
+
+	ListEventsForDay(day time.Time) ([]Event, error)
+	ListEventsForWeek(week time.Time) ([]Event, error)
+	ListEventsForMonth(month time.Month) ([]Event, error)
 }
 
 type Server interface {
