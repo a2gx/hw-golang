bccf6b4097dc5e45cf0e8439e80bc64d61459d9f
Alex Buylov <a.buylov@pnlfin.tech>
2025-04-08 16:34:51 +0300

    Hw12 in progress

--- hw12_13_14_15_16_calendar/internal/storage/postgres/storage.go
+++ hw12_13_14_15_16_calendar/internal/storage/postgres/storage.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/alxbuylov/hw-golang/hw12_13_14_15_calendar/internal/app"
 )
@@ -23,4 +24,34 @@ func (s *Storage) Connect(ctx context.Context) error {
 func (s *Storage) Close(ctx context.Context) error {
 	// TODO
 	return nil
+}
+
+func (s *Storage) CreateEvent(event app.Event) (app.Event, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Storage) UpdateEvent(event app.Event) (app.Event, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Storage) DeleteEvent(event app.Event) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Storage) ListEventsForDay(day time.Time) []app.Event {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Storage) ListEventsForWeek(week time.Time) []app.Event {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Storage) ListEventsForMonth(month time.Month) []app.Event {
+	//TODO implement me
+	panic("implement me")
 }
